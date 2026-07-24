13724fc8f284aba33d9d45f8d8e1ffdab99afecc
z4ky <firdauszaky17@gmail.com>
2026-07-24 17:53:30 +0700

    setup: prepare sqlite structure

--- src/database/sqlite.js
+++ src/database/sqlite.js
@@ -14,6 +14,17 @@ export async function initDB() {
 
   await db.open()
 
-  return db
+await db.execute(`
+CREATE TABLE IF NOT EXISTS wallet (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nama TEXT NOT NULL,
+    icon TEXT,
+    saldo REAL DEFAULT 0,
+    warna TEXT,
+    created_at TEXT
+);
+`)
+  
+    return db
 
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import {initDB} from './database/sqlite'
 
+async function startApp() {
+await initDB()
 createApp(App)
 .use(router)
 .mount('#app')
+}
+
+startApp()

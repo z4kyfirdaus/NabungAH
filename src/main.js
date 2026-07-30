e146fad799a19f231c3096bb269fd6e7bdcea338
z4ky <firdauszaky17@gmail.com>
2026-07-30 21:54:31 +0700

    Revisi ulang

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './style.css'
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+const app = createApp(App)
+
+// Penangkap error global untuk mendeteksi penyebab blank putih di HP
+app.config.errorHandler = (err, instance, info) => {
+  console.error('Global Error:', err);
+  document.body.innerHTML = `
+    <div style="padding: 20px; color: red; font-family: monospace; word-break: break-all;">
+      <h3>Terjadi Error Aplikasi:</h3>
+      <p>${err.toString()}</p>
+      <pre>${err.stack}</pre>
+    </div>
+  `;
+};
+
+app.use(router)
+app.mount('#app')
